2322f380845c57a5e6a593c2db0005698a775c0c
ankitdelhi353-collab <ankitdelhi353@gmail.com>
2026-06-09 17:38:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function searchAI() {
 
   result.innerHTML = "Thinking...";
 
-  const API_KEY = "AQ.Ab8RN6KlTFN4JDibnlV1OrUT04VxMhIc03-S97ZhIcX3SA3_0g";
+  const API_KEY ="AQ.Ab8RN6KlTFN4JDibnlV1OrUT04VxMhIc03-S97ZhIcX3SA3_0g";
 
   try {
     const response = await fetch(
